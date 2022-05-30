dcabde63fcd469700bac1ea4f7e0c605798aee77
Mateusz Giełżecki <mateuszgielzecki@gmail.com>
2022-05-31 00:04:27 +0200

    finish project

---
+++ script.js
@@ -0,0 +1,25 @@
+const keys = document.querySelectorAll('.key')
+
+const playKey = function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    console.log(key);
+    if (!audio) return
+
+    audio.currentTime = 0
+    audio.play();
+
+    key.classList.add('playing')
+}
+
+const removeTransition = function (e) {
+    if (e.propertyName !== 'transform') return
+    this.classList.remove('playing')
+}
+
+window.addEventListener('keydown', playKey)
+
+keys.forEach(key => {
+    key.addEventListener('transitionend', removeTransition)
+})
